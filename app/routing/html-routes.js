86743f2c24fed43a01a45c6c4186adf523e1acec
JenPhD <jpemberton76@gmail.com>
2016-09-25 21:27:11 -0400

    testing routes heroku

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -15,42 +15,16 @@ module.exports = function (app) {
 	});
 	app.get('/survey*', function (req, res) {
 		res.sendFile(path.join(__dirname, '..', 'public/survey.html'));
+	});
+	//Testing for heroku
+	app.get('/', function (req, res) {
+		//res.send("Hello heroku!");
+		res.sendFile(path.join(__dirname, '..', 'public/home.html'));	
+	});
 
 	//If no matching route is found default to home
 	app.use('/', function (req, res) {
 		res.sendFile(path.join(__dirname,'..', 'public/home.html'));
-	});
-
-	//Use static files in the public folder for css and images.
-	//app.use(express.static(__dirname + '/public'));
-	
-
-		// $('#submit').on('click', function () {
-		// 		var newFriend = {
-		// 			name: $('#name').val().trim(),
-		// 			phone: $('#photo').val().trim(),
-		// 			email: $('#email').val().trim(),
-		// 			scores: [
-		// 			$('#q1').val(),
-		// 			$('#q2').val(),
-		// 			$('#q3').val(),
-		// 			$('#q4').val(),
-		// 			$('#q5').val(),
-		// 			$('#q6').val(),
-		// 			$('#q7').val(),
-		// 			$('#q8').val(),
-		// 			$('#q9').val(),
-		// 			$('#q10').val()
-		// 			]
-					
-		// 		};
-		// 		$.post('http://localhost:3000/data/new', newFriend)
-		// 			.done(function (data) {
-		// 				console.log(data);
-		// 				alert('Adding friend...');
-		// 			});
-		// 		return false;
-		// });
 	});
 }
 
